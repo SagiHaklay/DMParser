bd4b73764b7add3bb40ffb6db9232c13f8aa14a5
Sagi Haklay <sahaklay@gmail.com>
2024-11-01 15:59:08 +0200

    added get_conditions method and tests

--- condition.py
+++ condition.py
@@ -1,13 +1,24 @@
 from anytree import AnyNode, find_by_attr
 
 class Condition:
-    def __init__(self, category, values, common=None) -> None:
+    def __init__(self, category, values, common=None, is_elsewhere=False) -> None:
         self.category = category
         self.values = values
         self.common = common
+        self.is_elsewhere = is_elsewhere
 
     def __repr__(self) -> str:
+        if self.is_elsewhere:
+            return f"Elsewhere: {self.category} not in {self.values}"
         return f"{self.category} in {self.values}"
+    
+
+def get_conditions(category, value_lists, common=None, with_elsewhere=False):
+    conditions = [Condition(category, values, common) for values in value_lists]
+    if with_elsewhere:
+        elsewhere_vals = [val for values in value_lists for val in values]
+        conditions.append(Condition(category, elsewhere_vals, common, True))
+    return conditions
 
 
 def check_condition(node: AnyNode, condition: Condition):
@@ -17,4 +28,6 @@ def check_condition(node: AnyNode, condition: Condition):
     if category_node is None or category_node.is_leaf:
         return False
     value_node = category_node.children[0]
+    if condition.is_elsewhere:
+        return value_node.tag not in condition.values
     return value_node.tag in condition.values
